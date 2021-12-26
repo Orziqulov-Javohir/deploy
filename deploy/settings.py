181e8a1d61959827cb1e8b3e6f5bd90deadc4d54
Orziqulov-Javohir <orziqulovjavohir16@gmail.com>
2021-12-26 19:16:06 +0500

    'second'

--- deploy/settings.py
+++ deploy/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 DEBUG = env.bool('DEBUG', default = False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
